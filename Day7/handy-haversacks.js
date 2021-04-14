7e90d05caea219d67fbb55129856e73b5d6c70a0
Jakkarin <invaderzgame@gmail.com>
2021-04-15 00:55:17 +0700

    [ADD] Day7 Part 1

---
+++ Day7/handy-haversacks.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+const luggages = fs.readFileSync("./Day7/input.txt").toString().split("\r\n");
+
+const createGraph = () => {
+  const map = new Map();
+  for (const luggage of luggages) {
+    const bags = luggage
+      .split(/\s*(?:bag)s{0,1}\.{0,1}(?: contain){0,1},{0,1}(?: \d){0,1}\s*/g)
+      .filter(Boolean);
+    map.set(bags[0], bags.slice(1));
+  }
+  return map;
+};
+
+const graph = createGraph();
+
+const dfs = (graph, startNode, searchNode) => {
+  const stack = [];
+  const visited = [];
+  stack.push(startNode);
+  visited.push(startNode);
+  while (stack.length != 0) {
+    let v = stack.pop();
+    for (const neighbor of graph.get(v)) {
+      if (!neighbor || neighbor !== "no other") {
+        if (v === searchNode) {
+          return true;
+        }
+        if (!visited.includes(neighbor)) {
+          stack.push(neighbor);
+          visited.push(neighbor);
+        }
+      }
+    }
+  }
+  return false;
+};
+
+let sum = 0;
+for (const luggage of graph.keys()) {
+  if (dfs(graph, luggage, "shiny gold") && luggage !== "shiny gold") {
+    sum = sum + 1;
+  }
+}
+
+console.log(sum);
