597486f824296aeca11d0721172a8bbff6748a4b
Jakkarin <invaderzgame@gmail.com>
2021-04-22 00:26:01 +0700

    [ADD] Day 9 Part 2

--- Day9/ecoding-error.js
+++ Day9/ecoding-error.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
-const encoded = fs.readFileSync("./Day9/test.txt").toString().split("\r\n");
+const encoded = fs.readFileSync("./Day9/input.txt").toString().split("\r\n");
 
-const windowSize = 5;
+const windowSize = 25;
 
 const findFirstInvalid = (encodedText) => {
   SKIP: for (let i = windowSize; i < encodedText.length; i++) {
@@ -11,9 +11,25 @@ const findFirstInvalid = (encodedText) => {
       dict[Math.abs(Number(encodedText[i]) - Number(number))] = Number(number);
       if (dict.hasOwnProperty(number)) continue SKIP;
     }
-    return encodedText[i];
+    return Number(encodedText[i]);
   }
 };
 
-const answer = findFirstInvalid(encoded);
-console.log(answer);
+const target = findFirstInvalid(encoded);
+console.log("part 1", target);
+
+const sumOfTarget = (target, encodedText) => {
+  SKIP2: for (const [i, firstNum] of encodedText.entries()) {
+    let sum = Number(firstNum);
+    for (let j = i + 1; j < encodedText.length; j++) {
+      sum += Number(encodedText[j]);
+      if (sum === Number(target))
+        return encodedText.slice(i, j + 1).map(Number);
+      if (sum > target) continue SKIP2;
+    }
+  }
+  return [];
+};
+
+const set = sumOfTarget(target, encoded);
+console.log("part 2", Math.max(...set) + Math.min(...set));
